ad579c497ff497e4f1f9a0c9b2dd9c9ad113b01e
JonathanPR24 <jonathanperez604@gmail.com>
2023-09-22 13:10:23 -0400

    reads and prints the content of a file

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -3,18 +3,11 @@
 
 const fs = require('fs');
 
-// Check if the user provided a file path as an argument
-if (process.argv.length !== 3) {
-  console.error('Usage: node 0-readme.js <file_path>');
-  process.exit(1);
-}
-
 const filePath = process.argv[2];
 
-// Read the file content using utf-8 encoding
-fs.readFile(filePath, 'utf-8', (err, data) => {
+fs.readFile(filePath, 'utf-8', function (err, data) {
   if (err) {
-    console.error(err);
+    console.log(err);
   } else {
     console.log(data);
   }

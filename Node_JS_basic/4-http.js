114599f10501c33904ae990ca3ab55ae985651b1
JonathanPR24 <jonathanperez604@gmail.com>
2023-10-14 00:29:06 -0400

    Create a small HTTP server using Node's HTTP module

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,11 +1,34 @@
+const { expect } = require('chai');
 const http = require('http');
+const app = require('./4-http');
 
-const app = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Holberton School!\n');
+describe('HTTP Server', () => {
+  let server;
+
+  before((done) => {
+    server = http.createServer(app);
+    server.listen(1245, () => {
+      done();
+    });
+  });
+
+  after(() => {
+    server.close();
+  });
+
+  it('should respond with "Hello Holberton School!" for /', (done) => {
+    http.get('http://localhost:1245', (response) => {
+      let data = '';
+
+      response.on('data', (chunk) => {
+        data += chunk;
+      });
+
+      response.on('end', () => {
+        expect(response.statusCode).to.equal(200);
+        expect(data).to.equal('Hello Holberton School!\n');
+        done();
+      });
+    });
+  });
 });
-
-app.listen(1245);
-
-module.exports = app;

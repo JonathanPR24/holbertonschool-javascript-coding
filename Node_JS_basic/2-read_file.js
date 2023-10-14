6cf0a5ec7e78dcc890be6693458f70dd0427ec08
JonathanPR24 <jonathanperez604@gmail.com>
2023-10-14 00:07:17 -0400

    Reading a file synchronously with Node JS

---
+++ Node_JS_basic/2-read_file.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  try {
+    // Read the contents of the database file synchronously
+    const fileContents = fs.readFileSync(path, 'utf8');
+
+    // Split the file contents into lines
+    const lines = fileContents.split('\n').filter((line) => line.trim() !== '');
+
+    // Initialize objects to store the counts of students in each field
+    const fieldCounts = {};
+
+    for (const line of lines) {
+      const data = line.split(',');
+
+      if (data.length === 4) {
+        const field = data[3].trim();
+
+        // Count the students in each field
+        if (fieldCounts[field]) {
+          fieldCounts[field].push(data[0].trim());
+        } else {
+          fieldCounts[field] = [data[0].trim()];
+        }
+      }
+    }
+
+    // Log the total number of students
+    const totalStudents = lines.length - 1; // Subtract 1 to exclude the header
+    console.log(`Number of students: ${totalStudents}`);
+
+    // Log the number of students in each field and the list of first names
+    for (const field in fieldCounts) {
+      const count = fieldCounts[field].length;
+      const list = fieldCounts[field].join(', ');
+      console.log(`Number of students in ${field}: ${count}. List: ${list}`);
+    }
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;

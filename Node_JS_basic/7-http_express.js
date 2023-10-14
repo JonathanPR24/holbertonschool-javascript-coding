36a62bbca4aebcd2b4ad4786d5204bcc64d360c7
JonathanPR24 <jonathanperez604@gmail.com>
2023-10-14 18:33:51 -0400

    Create a more complex HTTP server using Express

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,5 +1,10 @@
+/* eslint-disable */
 const express = require('express');
-const fs = require('fs').promises;
+
+const args = process.argv.slice(2);
+const countStudents = require('./3-read_file_async');
+
+const DATABASE = args[0];
 
 const app = express();
 const port = 1245;
@@ -9,47 +14,17 @@ app.get('/', (req, res) => {
 });
 
 app.get('/students', async (req, res) => {
+  const msg = 'This is the list of our students\n';
   try {
-    const databaseFilename = process.argv[2];
-    const fileContents = await fs.readFile(databaseFilename, 'utf8');
-    const lines = fileContents.split('\n').filter((line) => line.trim() !== '');
-    const fieldCounts = {};
-
-    for (const line of lines) {
-      const data = line.split(',');
-
-      if (data.length === 4) {
-        const field = data[3].trim();
-
-        if (Object.prototype.hasOwnProperty.call(fieldCounts, field)) {
-          fieldCounts[field].push(data[0].trim());
-        } else {
-          fieldCounts[field] = [data[0].trim()];
-        }
-      }
-    }
-
-    const totalStudents = lines.length - 1; // Subtract 1 to exclude the header
-    let response = 'This is the list of our students\n';
-    response += `Number of students: ${totalStudents}\n`;
-
-    for (const field in fieldCounts) {
-      if (Object.prototype.hasOwnProperty.call(fieldCounts, field)) {
-        const count = fieldCounts[field].length;
-        const list = fieldCounts[field].join(', ');
-        response += `Number of students in ${field}: ${count}. List: ${list}\n`;
-      }
-    }
-
-    res.send(response);
+    const students = await countStudents(DATABASE);
+    res.send(`${msg}${students.join('\n')}`);
   } catch (error) {
-    console.error(error);
-    res.status(500).send('This is the list of our students\nCannot load the database');
+    res.send(`${msg}${error.message}`);
   }
 });
 
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/`);
+  //   console.log(`Example app listening at http://localhost:${port}`);
 });
 
 module.exports = app;
